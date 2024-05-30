4ed576ba164e3d89e56eefbeb2fef2727dda3bcf
AllanCorderoLopez <62268148+AllanCorderoLopez@users.noreply.github.com>
2024-05-29 23:35:37 -0600

    navbar

--- threejs_portfolio/src/components/FloatingNav.tsx
+++ threejs_portfolio/src/components/FloatingNav.tsx
@@ -31,15 +31,7 @@ export const FloatingNav = ({
     if (typeof current === "number") {
       let direction = current! - scrollYProgress.getPrevious()!;
  
-      if (scrollYProgress.get() < 0.05) {
-        setVisible(true);
-      } else {
-        if (direction < 0) {
-          setVisible(true);
-        } else {
-          setVisible(true);
-        }
-      }
+      setVisible(true);
     }
   });
  
